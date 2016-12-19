c8a3787b1298ac4bbf028370ed778d28a31ae36f
videchen <vide.william@gmail.com>
2016-12-19 23:38:47 +0800

    Add Fragment system

---
+++ app/src/main/java/com/kidsdynamic/swing/androidswingapp/FragmentDevice.java
@@ -0,0 +1,28 @@
+package com.kidsdynamic.swing.androidswingapp;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by 03543 on 2016/12/19.
+ */
+
+public class FragmentDevice extends Fragment {
+
+    public View mMainView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mMainView = inflater.inflate(R.layout.fragment_device, container, false);
+
+        return mMainView;
+    }
+}
--- app/src/main/java/com/kidsdynamic/swing/androidswingapp/MainActivity.java
+++ app/src/main/java/com/kidsdynamic/swing/androidswingapp/MainActivity.java
@@ -1,13 +1,62 @@
 package com.kidsdynamic.swing.androidswingapp;
 
+import android.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private View mViewDevice;
+    private View mViewCalendar;
+    private View mViewDashboard;
+    private View mViewProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mViewDevice = findViewById(R.id.main_control_device);
+        mViewDevice.setOnClickListener(mControlClickListener);
+
+        mViewCalendar = findViewById(R.id.main_control_calendar);
+        mViewCalendar.setOnClickListener(mControlClickListener);
+
+        mViewDashboard = findViewById(R.id.main_control_dashboard);
+        mViewDashboard.setOnClickListener(mControlClickListener);
+
+        mViewProfile = findViewById(R.id.main_control_profile);
+        mViewProfile.setOnClickListener(mControlClickListener);
     }
+
+    private void showFragment(String className, Bundle args) {
+        Fragment fragment = Fragment.instantiate(this, className, args);
+        getFragmentManager()
+                .beginTransaction()
+                .replace(R.id.main_fragment, fragment, className)
+                .commit();
+    }
+
+    public void selectFragment(String className, Bundle args) {
+        showFragment(className, args);
+    }
+
+    View.OnClickListener mControlClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            if (view == mViewDevice) {
+                selectFragment(FragmentDevice.class.getName(), null);
+
+            } else if (view == mViewCalendar) {
+                selectFragment(FragmentCalendar.class.getName(), null);
+
+            } else if (view == mViewDashboard) {
+                selectFragment(FragmentDashboard.class.getName(), null);
+
+            } else if (view == mViewProfile) {
+                selectFragment(FragmentProfile.class.getName(), null);
+
+            }
+        }
+    };
 }
