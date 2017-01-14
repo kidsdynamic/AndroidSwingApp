57b53e09a373cba20c5dfa4ec70da919b1e502ce
videchen <vide.william@gmail.com>
2017-01-15 01:35:34 +0800

    Add photo clip in FragmentWatchKid

--- app/src/main/java/com/kidsdynamic/swing/androidswingapp/FragmentWatchKid.java
+++ app/src/main/java/com/kidsdynamic/swing/androidswingapp/FragmentWatchKid.java
@@ -1,6 +1,15 @@
 package com.kidsdynamic.swing.androidswingapp;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +19,9 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Created by 03543 on 2017/1/6.
  */
@@ -18,9 +30,17 @@ public class FragmentWatchKid extends ViewFragment {
     private ActivityMain mActivityMain;
     private View mViewMain;
 
+    private ViewPhoto mViewPhoto;
     private EditText mViewName;
     private EditText mViewZip;
     private ImageView mViewBack;
+
+    AlertDialog mDialog;
+
+    private Uri mPhotoUri;
+
+    public final static int ACTIVITY_RESULT_CAMERA_REQUEST = 1888;
+    public final static int ACTIVITY_RESULT_PHOTO_PICK = 9111;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,6 +51,9 @@ public class FragmentWatchKid extends ViewFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mViewMain = inflater.inflate(R.layout.fragment_watch_kid, container, false);
+
+        mViewPhoto = (ViewPhoto) mViewMain.findViewById(R.id.watch_kid_photo);
+        mViewPhoto.setOnClickListener(mPhotoClickListener);
 
         mViewName = (EditText) mViewMain.findViewById(R.id.watch_kid_name);
         mViewName.setOnEditorActionListener(mEdittextActionListener);
@@ -55,10 +78,78 @@ public class FragmentWatchKid extends ViewFragment {
         mActivityMain.popFragment();
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (!mActivityMain.mBitmapStack.isEmpty()) {
+            Bitmap bitmap = mActivityMain.mBitmapStack.pop();
+            mViewPhoto.setPhoto(bitmap);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (resultCode != Activity.RESULT_OK)
+            return;
+
+        Bitmap bitmap;
+        Uri uri;
+
+        if (requestCode == ACTIVITY_RESULT_CAMERA_REQUEST) {
+            uri = mPhotoUri;
+
+            mActivityMain.getContentResolver().notifyChange(uri, null);
+            ContentResolver cr = mActivityMain.getContentResolver();
+
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(cr, uri);
+                mActivityMain.mBitmapStack.push(bitmap);
+                mActivityMain.selectFragment(FragmentPhotoClip.class.getName(), null);
+            } catch (IOException e) {
+                Log.d("swing", "FragmentSignupKid(Camera):" + Log.getStackTraceString(e));
+            }
+
+        } else if (requestCode == ACTIVITY_RESULT_PHOTO_PICK) {
+            uri = intent.getData();
+
+            mActivityMain.getContentResolver().notifyChange(uri, null);
+            ContentResolver cr = mActivityMain.getContentResolver();
+
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(cr, uri);
+                mActivityMain.mBitmapStack.push(bitmap);
+                mActivityMain.selectFragment(FragmentPhotoClip.class.getName(), null);
+            } catch (IOException e) {
+                Log.d("swing", "FragmentSignupKid(Pick):" + Log.getStackTraceString(e));
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, intent);
+    }
+
     private View.OnClickListener mBackOnClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             onToolbarAction1();
+        }
+    };
+
+    private View.OnClickListener mPhotoClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            mDialog = new AlertDialog.Builder(mActivityMain).create();
+
+            LayoutInflater myLayout = LayoutInflater.from(mActivityMain);
+            View dialogView = myLayout.inflate(R.layout.dialog_photo, null);
+            mDialog.setView(dialogView);
+
+            (dialogView.findViewById(R.id.dialog_photo_take)).setOnClickListener(mDialogTakeClickListener);
+            (dialogView.findViewById(R.id.dialog_photo_library)).setOnClickListener(mDialogLibraryClickListener);
+            (dialogView.findViewById(R.id.dialog_photo_cancel)).setOnClickListener(mDialogCancelClickListener);
+
+            mDialog.setCancelable(false);
+            mDialog.show();
         }
     };
 
@@ -71,6 +162,50 @@ public class FragmentWatchKid extends ViewFragment {
             }
 
             return false;
+        }
+    };
+
+    private View.OnClickListener mDialogTakeClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            mDialog.dismiss();
+
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+            if (intent.resolveActivity(mActivityMain.getPackageManager()) != null) {
+                File photoFile = null;
+                File storageDir;
+                try {
+                    storageDir = mActivityMain.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+                    photoFile = File.createTempFile("photo", ".jpg", storageDir);
+                    boolean detr = photoFile.delete();
+                } catch (IOException e) {
+                    Log.d("swing", "mDialogTakeClickListener Exception: " + Log.getStackTraceString(e));
+                }
+
+                if (photoFile != null) {
+                    mPhotoUri = Uri.fromFile(photoFile);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, mPhotoUri);
+                    startActivityForResult(intent, ACTIVITY_RESULT_CAMERA_REQUEST);
+                }
+            }
+        }
+    };
+
+    private View.OnClickListener mDialogLibraryClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            mDialog.dismiss();
+
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(Intent.createChooser(intent, "Select Picture"), ACTIVITY_RESULT_PHOTO_PICK);
+        }
+    };
+
+    private View.OnClickListener mDialogCancelClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            mDialog.dismiss();
         }
     };
 }
