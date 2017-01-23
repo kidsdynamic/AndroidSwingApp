03ee49e6866e54e2deb0bf3646dcd0bf041cee26
WeiChi <weichi.gio@gmail.com>
2017-01-24 02:14:20 +0800

    Send JSON as parameter for method of POST and PUT.

---
+++ app/src/main/java/com/kidsdynamic/swing/androidswingapp/ServerMachineCore.java
@@ -0,0 +1,123 @@
+package com.kidsdynamic.swing.androidswingapp;
+
+import android.content.Context;
+
+import com.android.volley.Request;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by weichigio on 2017/1/24.
+ */
+
+public class ServerMachineCore extends ServerMachine {
+    public ServerMachineCore(Context context) {
+        super(context);
+    }
+
+    Runnable responser = new Runnable() {
+        @Override
+        public void run() {
+            if (mResponse != null) {
+                mResponse.onResponse(true, 200, "");
+            }
+        }
+    };
+
+    ResponseListener mResponse = null;
+
+    @Override
+    public void userLogin(ResponseListener response, String email, String password) {
+        mHandler.postDelayed(responser, 1000);
+    }
+
+    @Override
+    public void userRegister(ResponseListener response, String email, String password, String firstName, String lastName, String phoneNumber, String zipCode) {
+    }
+
+    @Override
+    public void userIsTokenValid(ResponseListener response, String email, String token) {
+    }
+
+    @Override
+    public void userIsMailAvailableToRegister(ResponseListener response, String email) {
+    }
+
+    @Override
+    public void userUpdateProfile(ResponseListener response, String firstName, String lastName, String phoneNumber, String zipCode) {
+    }
+
+    @Override
+    public void userRetrieveUserProfile(ResponseListener response) {
+    }
+
+    @Override
+    public void userAvatarUpload(ResponseListener response, String filePath) {
+    }
+
+    @Override
+    public void userAvatarUploadKid(ResponseListener response, String kidId, String filePath) {
+    }
+
+    @Override
+    public void kidsAdd(ResponseListener response, String firstName, String lastName, String macId) {
+    }
+
+    @Override
+    public void kidsUpdate(ResponseListener response, String kidId, String firstName, String lastName) {
+    }
+
+    @Override
+    public void kidsWhoRegisteredMacID(ResponseListener response, String macId) {
+    }
+
+    @Override
+    public void activityUploadRawData(ResponseListener response, String indoorActivity, String outdoorActivity, String time, String macId) {
+    }
+
+    @Override
+    public void activityRetrieveData(ResponseListener response, String kidId, String period) {
+    }
+
+    //@Override
+    //public void eventAdd(ResponseListener response, int kidId, String name, String startDate, String endDate,
+    //                     String color, String description, String alert, String city, String state, String repeat,
+    //                     int timezoneOffset, String todo) {
+    //}
+
+    @Override
+    public void eventUpdate(ResponseListener response, String eventId, String name, String startDate, String endDate,
+                            String color, String description, String alert, String city, String state, String repeat,
+                            String timezoneOffset, String todo) {
+    }
+
+    @Override
+    public void eventDelete(ResponseListener response, String eventId) {
+    }
+
+    @Override
+    public void eventRetrieveEvents(ResponseListener response, String period, String date) {
+    }
+
+    @Override
+    public void eventRetrieveAllEventsWithTodo(ResponseListener response) {
+    }
+
+    @Override
+    public void subhostAdd(ResponseListener response, String hostId) {
+    }
+
+    @Override
+    public void subhostAccept(ResponseListener response, String subHostId, String KidId) {
+    }
+
+    @Override
+    public void subhostDeny(ResponseListener response, String subHostId) {
+    }
+
+    @Override
+    public void subhostList(ResponseListener response, String status) {
+    }
+
+}
