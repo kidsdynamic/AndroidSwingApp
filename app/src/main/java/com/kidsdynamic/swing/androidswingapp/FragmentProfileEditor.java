c8fbc4effe84e184d68442629152393375e18ac5
videchen <vide.william@gmail.com>
2017-02-10 01:07:12 +0800

    Enable photo editor in FragmentProfileEditor

--- app/src/main/java/com/kidsdynamic/swing/androidswingapp/FragmentProfileEditor.java
+++ app/src/main/java/com/kidsdynamic/swing/androidswingapp/FragmentProfileEditor.java
@@ -1,10 +1,22 @@
 package com.kidsdynamic.swing.androidswingapp;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by 03543 on 2017/1/24.
@@ -19,6 +31,11 @@ public class FragmentProfileEditor extends ViewFragment {
     private EditText mViewFirst;
     private EditText mViewLast;
 
+    AlertDialog mDialog;
+    private Uri mPhotoUri;
+    public final static int ACTIVITY_RESULT_CAMERA_REQUEST = 1888;
+    public final static int ACTIVITY_RESULT_PHOTO_PICK = 9111;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +47,7 @@ public class FragmentProfileEditor extends ViewFragment {
         mViewMain = inflater.inflate(R.layout.fragment_profile_editor, container, false);
 
         mViewPhoto = (ViewCircle) mViewMain.findViewById(R.id.profile_editor_photo);
+        mViewPhoto.setOnClickListener(mPhotoListener);
 
         mViewFirst = (EditText)
                 mViewMain.findViewById(R.id.profile_editor_first).
@@ -49,4 +67,131 @@ public class FragmentProfileEditor extends ViewFragment {
         mActivityMain.popFragment();
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (!mActivityMain.mBitmapStack.isEmpty()) {
+            mViewPhoto.setBitmap(mActivityMain.mBitmapStack.pop());
+        } else {
+            profileLoad();
+        }
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        profileSave();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (resultCode != Activity.RESULT_OK)
+            return;
+
+        Bitmap bitmap;
+        Uri uri;
+
+        if (requestCode == ACTIVITY_RESULT_CAMERA_REQUEST) {
+            uri = mPhotoUri;
+
+            mActivityMain.getContentResolver().notifyChange(uri, null);
+            ContentResolver cr = mActivityMain.getContentResolver();
+
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(cr, uri);
+                mActivityMain.mBitmapStack.push(bitmap);
+                mActivityMain.selectFragment(FragmentPhotoClip.class.getName(), null);
+            } catch (IOException e) {
+                Log.d("swing", "FragmentSignupProfile(Camera):" + Log.getStackTraceString(e));
+            }
+
+        } else if (requestCode == ACTIVITY_RESULT_PHOTO_PICK) {
+            uri = intent.getData();
+
+            mActivityMain.getContentResolver().notifyChange(uri, null);
+            ContentResolver cr = mActivityMain.getContentResolver();
+
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(cr, uri);
+                mActivityMain.mBitmapStack.push(bitmap);
+                mActivityMain.selectFragment(FragmentPhotoClip.class.getName(), null);
+            } catch (IOException e) {
+                Log.d("swing", "FragmentSignupProfile(Pick):" + Log.getStackTraceString(e));
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, intent);
+    }
+
+    private View.OnClickListener mPhotoListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            mDialog = new AlertDialog.Builder(mActivityMain).create();
+
+            LayoutInflater myLayout = LayoutInflater.from(mActivityMain);
+            View dialogView = myLayout.inflate(R.layout.dialog_photo, null);
+            mDialog.setView(dialogView);
+
+            (dialogView.findViewById(R.id.dialog_photo_take)).setOnClickListener(mDialogTakeClickListener);
+            (dialogView.findViewById(R.id.dialog_photo_library)).setOnClickListener(mDialogLibraryClickListener);
+            (dialogView.findViewById(R.id.dialog_photo_cancel)).setOnClickListener(mDialogCancelClickListener);
+
+            mDialog.setCancelable(false);
+            mDialog.show();
+        }
+    };
+
+    private View.OnClickListener mDialogTakeClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            mDialog.dismiss();
+
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+            if (intent.resolveActivity(mActivityMain.getPackageManager()) != null) {
+                File photoFile = null;
+                File storageDir;
+                try {
+                    storageDir = mActivityMain.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+                    photoFile = File.createTempFile("photo", ".jpg", storageDir);
+                    boolean detr = photoFile.delete();
+                } catch (IOException e) {
+                    Log.d("swing", "mDialogTakeClickListener Exception: " + Log.getStackTraceString(e));
+                }
+
+                if (photoFile != null) {
+                    mPhotoUri = Uri.fromFile(photoFile);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, mPhotoUri);
+                    startActivityForResult(intent, ACTIVITY_RESULT_CAMERA_REQUEST);
+                }
+            }
+        }
+    };
+
+    private View.OnClickListener mDialogLibraryClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            mDialog.dismiss();
+
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(Intent.createChooser(intent, "Select Picture"), ACTIVITY_RESULT_PHOTO_PICK);
+        }
+    };
+
+    private View.OnClickListener mDialogCancelClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            mDialog.dismiss();
+        }
+    };
+
+    private void profileLoad() {
+
+    }
+
+    private void profileSave() {
+
+    }
 }
