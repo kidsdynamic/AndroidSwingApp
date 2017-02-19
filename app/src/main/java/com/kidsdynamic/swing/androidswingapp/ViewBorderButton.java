368fa0a9ac8209d7e82c054cafa12f3e9facf849
videchen <vide.william@gmail.com>
2017-02-19 17:34:33 +0800

    Add missed files

---
+++ app/src/main/java/com/kidsdynamic/swing/androidswingapp/ViewBorderButton.java
@@ -0,0 +1,202 @@
+package com.kidsdynamic.swing.androidswingapp;
+
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.Paint;
+import android.graphics.PixelFormat;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.StateListDrawable;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.widget.Button;
+
+/**
+ * Created by 03543 on 2017/2/19.
+ */
+
+public class ViewBorderButton extends Button {
+    private int mFloorColor = Color.GRAY;
+    private int mBorderColor = Color.WHITE;
+    private int mBorderWidth = 20;
+
+    public ViewBorderButton(Context context) {
+        super(context);
+        init(context, null);
+    }
+
+    public ViewBorderButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    public ViewBorderButton(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+
+        if (attrs != null) {
+            TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.ViewBorderButton);
+
+            final int count = typedArray.getIndexCount();
+            for (int idx = 0; idx < count; idx++) {
+                final int attr = typedArray.getIndex(idx);
+
+                if (attr == R.styleable.ViewBorderButton_floorColor) {
+                    mFloorColor = typedArray.getColor(attr, mFloorColor);
+                } else if (attr == R.styleable.ViewBorderButton_borderColor) {
+                    mBorderColor = typedArray.getColor(attr, mBorderColor);
+                } else if (attr == R.styleable.ViewBorderButton_borderWidth) {
+                    mBorderWidth = typedArray.getDimensionPixelSize(attr, mBorderWidth);
+                }
+            }
+            typedArray.recycle();
+        }
+
+        makeAppearance(mFloorColor, mBorderColor, mBorderWidth);
+    }
+
+    public void makeAppearance(int floorColor, int borderColor, int strokeWidth) {
+        StateListDrawable background = new StateListDrawable();
+
+        background.addState(new int[]{android.R.attr.state_pressed}, new BackgroundDrawable(borderColor, floorColor, strokeWidth));
+        background.addState(new int[]{}, new BackgroundDrawable(floorColor, borderColor, strokeWidth));
+
+        setBackground(background);
+
+        int[][] states = new int[][]{
+                new int[]{android.R.attr.state_pressed},
+                new int[]{}
+        };
+
+        int[] colors = new int[]{
+                Color.GRAY,
+                borderColor,
+        };
+
+        setTextColor(new ColorStateList(states, colors));
+    }
+
+    public void setFloorColor(int color) {
+        mFloorColor = color;
+        makeAppearance(mFloorColor, mBorderColor, mBorderWidth);
+    }
+
+    public int getFloorColor() {
+        return mFloorColor;
+    }
+
+    public void setBorderColor(int color) {
+        mBorderColor = color;
+        makeAppearance(mFloorColor, mBorderColor, mBorderWidth);
+    }
+
+    public int getBorderColor() {
+        return mBorderColor;
+    }
+
+    private class BackgroundDrawable extends Drawable {
+        private Paint mPaint;
+
+        int mFloorColor = Color.GRAY;
+        int mBorderColor = Color.WHITE;
+
+        public BackgroundDrawable() {
+            int strokeWidth = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                    5, getResources().getDisplayMetrics());
+
+            init(mFloorColor, mBorderColor, strokeWidth);
+        }
+
+        public BackgroundDrawable(int floorColor, int borderColor) {
+            int strokeWidth = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                    5, getResources().getDisplayMetrics());
+
+            init(floorColor, borderColor, strokeWidth);
+        }
+
+        public BackgroundDrawable(int floorColor, int borderColor, int strokeWidth) {
+            init(floorColor, borderColor, strokeWidth);
+        }
+
+        private void init(int floorColor, int borderColor, int strokeWidth) {
+            mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+            mPaint.setStrokeWidth(strokeWidth);
+            mPaint.setStyle(Paint.Style.FILL);
+
+            mFloorColor = floorColor;
+            mBorderColor = borderColor;
+        }
+
+        @Override
+        public void draw(Canvas canvas) {
+            Rect bound = getBounds();
+            RectF rect = new RectF(0, 0, bound.width(), bound.height());
+
+            int round = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                    10, getResources().getDisplayMetrics());
+
+            if (mBorderWidth > 0) {
+                mPaint.setColor(mBorderColor);
+                canvas.drawRoundRect(rect, round, round, mPaint);
+            }
+
+            rect.set(rect.left + mBorderWidth, rect.top + mBorderWidth,
+                    rect.right - mBorderWidth, rect.bottom - mBorderWidth);
+            mPaint.setColor(mFloorColor);
+            canvas.drawRoundRect(rect, round, round, mPaint);
+        }
+
+        @Override
+        protected boolean onLevelChange(int level) {
+            invalidateSelf();
+            return true;
+        }
+
+        @Override
+        public void setAlpha(int alpha) {
+            mPaint.setAlpha(alpha);
+        }
+
+        @Override
+        public void setColorFilter(ColorFilter colorFilter) {
+            mPaint.setColorFilter(colorFilter);
+        }
+
+        @Override
+        public int getOpacity() {
+            return PixelFormat.TRANSLUCENT;
+        }
+
+        public void setFloorColor(int color) {
+            mFloorColor = color;
+        }
+
+        public int getFloorColor() {
+            return mFloorColor;
+        }
+
+        public void setBorderColor(int color) {
+            mBorderColor = color;
+        }
+
+        public int getBorderColor() {
+            return mBorderColor;
+        }
+
+        public void setStrokeWidth(float strokeWidth) {
+            mPaint.setStrokeWidth(strokeWidth);
+        }
+
+        public float getStrokeWidth() {
+            return mPaint.getStrokeWidth();
+        }
+    }
+}
