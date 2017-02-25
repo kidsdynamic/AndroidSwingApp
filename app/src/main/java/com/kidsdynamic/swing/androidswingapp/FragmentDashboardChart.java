5637558b5fac2d1da238583359f0934744dfe796
videchen <vide.william@gmail.com>
2017-02-26 00:38:26 +0800

    Translate for FragmentDashboardChart

--- app/src/main/java/com/kidsdynamic/swing/androidswingapp/FragmentDashboardChart.java
+++ app/src/main/java/com/kidsdynamic/swing/androidswingapp/FragmentDashboardChart.java
@@ -6,9 +6,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.TranslateAnimation;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
